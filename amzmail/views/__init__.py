d3012fbbed19d66380d1032eddd57d13fcae7acf
kienchuong <136041187+kienchuong@users.noreply.github.com>
2026-07-21 12:26:39 +0700

    Prepare 0.3.0: add amzmail/views/__init__.py

---
+++ amzmail/views/__init__.py
@@ -0,0 +1,16 @@
+"""CustomTkinter views for Amazon Mail Reader."""
+
+from .accounts import AccountsViewMixin
+from .inbox import InboxViewMixin
+from .payments import PaymentsViewMixin
+from .settings import SettingsViewMixin
+from .shell import ShellViewMixin
+
+__all__ = [
+    "AccountsViewMixin",
+    "InboxViewMixin",
+    "PaymentsViewMixin",
+    "SettingsViewMixin",
+    "ShellViewMixin",
+]
+
